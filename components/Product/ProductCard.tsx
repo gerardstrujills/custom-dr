1d63c4e6cd73a74d4290f8a60551d24155ca8057
gerardstrujills <gtrujillo@cfbd.co>
2026-01-18 22:03:50 -0500

    add upload access

--- components/Product/ProductCard.tsx
+++ components/Product/ProductCard.tsx
@@ -229,8 +229,8 @@ const ProductCard = ({ pathname, data, user }: Props) => {
             <div className="s-post-summary--content w100">
               <div className="s-post-summary--content-title mb2 d-flex">
                 <div
-                  // href={`/products/[productId]`}
-                  // as={`/products/${q.id}`}
+                // href={`/products/[productId]`}
+                // as={`/products/${q.id}`}
                 >
                   <a className="s-link">{q.title}</a>
                 </div>
@@ -260,7 +260,7 @@ const ProductCard = ({ pathname, data, user }: Props) => {
                 product={q}
               />
 
-              {user.me?.isAccess && (
+              {user.me?.isAccess ? (
                 <div className="s-post-summary--meta mt4">
                   <div
                     onClick={() => setSupplier(q.id)}
@@ -298,6 +298,27 @@ const ProductCard = ({ pathname, data, user }: Props) => {
                         Eliminar
                       </div>
                     </div>
+                  </div>
+                </div>
+              ) : (
+                <div className="s-post-summary--meta mt4">
+                  <div
+                    onClick={() => setSupplier(q.id)}
+                    className="s-link s-user-card--link d-flex gs4 ml4"
+                  >
+                    <svg
+                      aria-hidden="true"
+                      focusable="false"
+                      role="img"
+                      className="Octicon-sc-9kayk9-0"
+                      viewBox="0 0 16 16"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                    >
+                      <path d="M1.75 16A1.75 1.75 0 0 1 0 14.25V1.75C0 .784.784 0 1.75 0h8.5C11.216 0 12 .784 12 1.75v12.5c0 .085-.006.168-.018.25h2.268a.25.25 0 0 0 .25-.25V8.285a.25.25 0 0 0-.111-.208l-1.055-.703a.749.749 0 1 1 .832-1.248l1.055.703c.487.325.779.871.779 1.456v5.965A1.75 1.75 0 0 1 14.25 16h-3.5a.766.766 0 0 1-.197-.026c-.099.017-.2.026-.303.026h-3a.75.75 0 0 1-.75-.75V14h-1v1.25a.75.75 0 0 1-.75.75Zm-.25-1.75c0 .138.112.25.25.25H4v-1.25a.75.75 0 0 1 .75-.75h2.5a.75.75 0 0 1 .75.75v1.25h2.25a.25.25 0 0 0 .25-.25V1.75a.25.25 0 0 0-.25-.25h-8.5a.25.25 0 0 0-.25.25ZM3.75 6h.5a.75.75 0 0 1 0 1.5h-.5a.75.75 0 0 1 0-1.5ZM3 3.75A.75.75 0 0 1 3.75 3h.5a.75.75 0 0 1 0 1.5h-.5A.75.75 0 0 1 3 3.75Zm4 3A.75.75 0 0 1 7.75 6h.5a.75.75 0 0 1 0 1.5h-.5A.75.75 0 0 1 7 6.75ZM7.75 3h.5a.75.75 0 0 1 0 1.5h-.5a.75.75 0 0 1 0-1.5ZM3 9.75A.75.75 0 0 1 3.75 9h.5a.75.75 0 0 1 0 1.5h-.5A.75.75 0 0 1 3 9.75ZM7.75 9h.5a.75.75 0 0 1 0 1.5h-.5a.75.75 0 0 1 0-1.5Z"></path>
+                    </svg>
+                    <div className="flex--item -link">Proveedor</div>
                   </div>
                 </div>
               )}
